d608f23b1138aeaeff7a3056933e1483dc7c347f
NothingAroundUs <husseintaha756@gmail.com>
2025-01-02 07:33:30 -0800

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, AboutComponent, ContactComponent, NavComponent, FooterComponent, PortfolioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
